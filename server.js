039d0f05ddb85ce61d470e2f5318a8b51e8be5a4
Jeremy Ellis <keyfreemusic@gmail.com>
2022-12-17 20:54:56 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 console.log("Server started");
 var Msg = '';
 var WebSocketServer = require('ws').Server
-    , wss = new WebSocketServer({port: 80});
+    , wss = new WebSocketServer({port: process.env.PORT || 8080});
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
         console.log('Received from client: %s', message);
